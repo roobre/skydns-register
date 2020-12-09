ea536a2bace745c8ce94842dc3e5168b10aa0c58
Roberto Santalla <roobre@roobre.es>
2020-12-09 20:33:14 +0100

    etcdclient: fix parsing and updating

--- etcdclient.py
+++ etcdclient.py
@@ -1,5 +1,6 @@
 import etcd3
 import logging
+import json
 
 
 class EtcdClient:
@@ -7,17 +8,18 @@ class EtcdClient:
         self._dry_run = dry_run
         self._etcd = etcd3.client(host, port)
         if not self._dry_run:
-            self._etcd.get('')
+            self._etcd.get('/')
 
     def update(self, records: dict):
-        for oldk, oldv in self._etcd.get_all():
-            if 'managed-by' in oldv and oldv['managed-by'] == 'skydns-register' and oldk not in records:
-                logging.info(f"Deleting {oldk}")
+        for val, meta in self._etcd.get_all():
+            val = json.loads(val)
+            if 'managed-by' in val and val['managed-by'] == 'skydns-register' and meta.key not in records:
+                logging.info(f"Deleting {meta.key}")
                 if not self._dry_run:
-                    self._etcd.delete(oldk)
+                    self._etcd.delete(meta.key)
 
         for path in records:
-            record = records['key']
+            record = records[path]
             logging.info(f"Putting {path}")
             if not self._dry_run:
-                self._etcd.put(path, record)
+                self._etcd.put(path, json.dumps(record))
