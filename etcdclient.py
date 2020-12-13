3debf59c33e6a8eaef07613fecd0110095b15632
kangco-de <53659978+kangco-de@users.noreply.github.com>
2020-12-13 20:55:14 +0100

    Add parametric owner id to all records (#1)
    * add a parametric owner id to all records
    
    * Update etcdclient.py
    
    Co-authored-by: Roberto Santalla <roobre@users.noreply.github.com>
    
    Co-authored-by: Juan Manuel Pérez Flórez <devel@kangco.de>
    Co-authored-by: Roberto Santalla <roobre@users.noreply.github.com>

--- etcdclient.py
+++ etcdclient.py
@@ -4,7 +4,8 @@ import json
 
 
 class EtcdClient:
-    def __init__(self, host: str, port: int, dry_run: bool = False):
+    def __init__(self, host: str, port: int, dry_run: bool = False, owner_id: str = 'skyreg'):
+        self._owner_id = owner_id
         self._dry_run = dry_run
         self._etcd = etcd3.client(host, port)
         if not self._dry_run:
@@ -13,13 +14,14 @@ class EtcdClient:
     def update(self, records: dict):
         for val, meta in self._etcd.get_all():
             val = json.loads(val)
-            if 'managed-by' in val and val['managed-by'] == 'skydns-register' and meta.key.decode() not in records:
+            if 'managed-by' in val and val['managed-by'] == self._owner_id and meta.key.decode() not in records:
                 logging.info(f"Deleting {meta.key}")
                 if not self._dry_run:
                     self._etcd.delete(meta.key)
 
         for path in records:
             record = records[path]
+            record['managed-by'] = self._owner_id
             logging.info(f"Putting {path}")
             if not self._dry_run:
                 self._etcd.put(path, json.dumps(record))
